17675c2c83d4e537dbe6208d8106841dbcf9cb58
Guy Cohen <guycoh@outlook.co.il>
2025-05-29 21:09:52 +0300

    עדכון אחרון

--- app/api/pexels-images/[id]/route.ts
+++ app/api/pexels-images/[id]/route.ts
@@ -1,23 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const { id } = params;
-  const res = await fetch(`https://api.pexels.com/v1/photos/${id}`, {
-    headers: {
-      Authorization: process.env.PEXELS_API_KEY || '',
-    },
-  });
+// export async function GET(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const { id } = params;
+//   const res = await fetch(`https://api.pexels.com/v1/photos/${id}`, {
+//     headers: {
+//       Authorization: process.env.PEXELS_API_KEY || '',
+//     },
+//   });
 
-  if (!res.ok) {
-    return NextResponse.json(
-      { error: 'Failed to fetch photo details', status: res.status },
-      { status: res.status }
-    );
-  }
+//   if (!res.ok) {
+//     return NextResponse.json(
+//       { error: 'Failed to fetch photo details', status: res.status },
+//       { status: res.status }
+//     );
+//   }
 
-  const data = await res.json();
-  return NextResponse.json(data);
-}
+//   const data = await res.json();
+//   return NextResponse.json(data);
+// }
