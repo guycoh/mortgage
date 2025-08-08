211c35e192fb597f8bbad47f9ac9482960a3c5c7
Guy Cohen <guycoh@outlook.co.il>
2025-08-08 12:36:21 +0300

    עדכון אחרון

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
-//תיקון 02/08/2025 בעיית canva
+//מקורי
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // למנוע מ־pdfjs-dist לנסות לפתור את המודול native 'canvas' בצד קליינט
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        canvas: false,
-      };
-    }
-    return config;
-  },
-  // אם אתה משתמש ב־app directory (Next.js 13+), אין צורך לשנות פה משהו נוסף בהקשר הזה
+  /* config options here */
 };
 
 export default nextConfig;
@@ -22,17 +14,28 @@ export default nextConfig;
 
 
 
-
-
-
-
-//מקורי
-
-
+//תיקון 02/08/2025 בעיית canva
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   /* config options here */
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       // למנוע מ־pdfjs-dist לנסות לפתור את המודול native 'canvas' בצד קליינט
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         canvas: false,
+//       };
+//     }
+//     return config;
+//   },
+//   // אם אתה משתמש ב־app directory (Next.js 13+), אין צורך לשנות פה משהו נוסף בהקשר הזה
 // };
 
 // export default nextConfig;
+
+
+
+
+
+
+
