9605c705fe1754f380e5c677d88698e9fa4b8ef7
Guy Cohen <guycoh@outlook.co.il>
2026-03-28 11:19:49 +0300

    עדכון אחרון

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,104 +1,104 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config = {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-       // main: "#6929AC", סגול מוני
-       // main:"#1d75a1",  מורגי
-        main:" #084F99",
+// const config = {
+//   darkMode: ["class"],
+//   content: [
+//     "./pages/**/*.{ts,tsx}",
+//     "./components/**/*.{ts,tsx}",
+//     "./app/**/*.{ts,tsx}",
+//     "./src/**/*.{ts,tsx}",
+//   ],
+//   prefix: "",
+//   theme: {
+//     container: {
+//       center: true,
+//       padding: "2rem",
+//       screens: {
+//         "2xl": "1400px",
+//       },
+//     },
+//     extend: {
+//       colors: {
+//        // main: "#6929AC", סגול מוני
+//        // main:"#1d75a1",  מורגי
+//         main:" #084F99",
      
-        second: "#F59E0B",
+//         second: "#F59E0B",
         
-       footer: "#a39d8f",
-        //footer: "#6929AC",
-        fonts: "#475569",
-        fontss1: "#546e7a",
+//        footer: "#a39d8f",
+//         //footer: "#6929AC",
+//         fonts: "#475569",
+//         fontss1: "#546e7a",
        
-       galbg: "#e5e4e3",
+//        galbg: "#e5e4e3",
        
-        // galbg: "#EAE5FC",
+//         // galbg: "#EAE5FC",
 
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "tracking-in-expand-fwd": {
-          "0%": {
-            letterSpacing: "-0.5em",
-            transform: "translateZ(-700px)",
-            opacity: "0",
-          },
-          "40%": {
-            opacity: "0.6",
-          },
-          "100%": {
-            transform: "translateZ(0)",
-            opacity: "1",
-          },
-        },
-      },
-      animation: {
-        "tracking-in-expand-fwd":
-          "tracking-in-expand-fwd 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+//         border: "hsl(var(--border))",
+//         input: "hsl(var(--input))",
+//         ring: "hsl(var(--ring))",
+//         background: "hsl(var(--background))",
+//         foreground: "hsl(var(--foreground))",
+//         primary: {
+//           DEFAULT: "hsl(var(--primary))",
+//           foreground: "hsl(var(--primary-foreground))",
+//         },
+//         secondary: {
+//           DEFAULT: "hsl(var(--secondary))",
+//           foreground: "hsl(var(--secondary-foreground))",
+//         },
+//         destructive: {
+//           DEFAULT: "hsl(var(--destructive))",
+//           foreground: "hsl(var(--destructive-foreground))",
+//         },
+//         muted: {
+//           DEFAULT: "hsl(var(--muted))",
+//           foreground: "hsl(var(--muted-foreground))",
+//         },
+//         accent: {
+//           DEFAULT: "hsl(var(--accent))",
+//           foreground: "hsl(var(--accent-foreground))",
+//         },
+//         popover: {
+//           DEFAULT: "hsl(var(--popover))",
+//           foreground: "hsl(var(--popover-foreground))",
+//         },
+//         card: {
+//           DEFAULT: "hsl(var(--card))",
+//           foreground: "hsl(var(--card-foreground))",
+//         },
+//       },
+//       borderRadius: {
+//         lg: "var(--radius)",
+//         md: "calc(var(--radius) - 2px)",
+//         sm: "calc(var(--radius) - 4px)",
+//       },
+//       keyframes: {
+//         "tracking-in-expand-fwd": {
+//           "0%": {
+//             letterSpacing: "-0.5em",
+//             transform: "translateZ(-700px)",
+//             opacity: "0",
+//           },
+//           "40%": {
+//             opacity: "0.6",
+//           },
+//           "100%": {
+//             transform: "translateZ(0)",
+//             opacity: "1",
+//           },
+//         },
+//       },
+//       animation: {
+//         "tracking-in-expand-fwd":
+//           "tracking-in-expand-fwd 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// } satisfies Config;
 
-export default config;
+// export default config;
 
 
 
